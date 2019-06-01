324237838a89cad51426ff68f18c23123163edd0
Knaak53 <manugara53@gmail.com>
2019-06-01 21:00:01 +0200

    Finish animation, stop open doors, and clear weld prop properly fixed

--- __resource.lua
+++ __resource.lua
@@ -1,11 +1,21 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+dependency 'essentialmode'
+dependency 'es_extended'
+dependency 'esx_doorlock'
+dependency 'esx_blowtorch'
+dependency 'mhacking'
+client_scripts {
 
-description 'ESX Borrmaskin'
-
-server_scripts {
-	'server/main.lua'
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'config.lua',
+	'client/client.lua'
 }
 
-client_scripts {
-	'client/main.lua'
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'config.lua',
+	'server/server.lua'
 }
